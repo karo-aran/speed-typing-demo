ef8b4e946f0e683c70aa5867a3b16e17b0483d51
Colleen-L <nonexistingalien@gmail.com>
2025-06-16 14:24:10 -0400

    updating starting js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-// define variables
+// declare and define variables that refer to HTML elements
 
 
+// running time of the timer (during an iteration when the value changes)
+// necessary for countdown functionality
+let curr_time = document.querySelector(".time");
 
+let seconds = 60;
+let points = 0;
 
-
-//??
-
-
-
-
-// define functions
+// variable that holds the letter typed
+let typed;
+// spans hold individual letters of the test word
+let spans;
