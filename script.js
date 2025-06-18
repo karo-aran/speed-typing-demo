1f8c8b52a39763735f9420821c651d25b44bd988
karolynnaran@gmail.com <karolynnaran@gmail.com>
2025-06-17 21:16:10 -0400

    yippeee

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // declare and define variables that refer to HTML elements
-
+let btn = document.querySelector('.startBtn');
+let time = document.querySelector('.time');
+let score = document.querySelector('.score');
+let test = document.querySelector('.test');
 
 // running time of the timer (during an iteration when the value changes)
 // necessary for countdown functionality
@@ -17,14 +20,37 @@ let game_active = false;
 
 // make your list of test words here
 // you may change the existing words
-let list = ["the","quick","brown","fox","jumps","over","the","lazy","dog"];
+let list = ["strawberry","banana","purple","pink","elegant","amor","cutie","sleepy","live"];
 
 /*
  *
  * countdown()
  * 
  */
+    function countdown() {
+        timer = setInterval( function() {
+            seconds -= 1;
+            curr_time.innerHTML = seconds;
 
+            // timer ends
+            if (seconds <=0) {
+                //ending alert
+                alert("Game Over! Your score was " + points);
+
+                // rests ALL HTML elements
+                score.innerHTML = "0";
+                test.innerHTML = "";
+                time.innerHTML = "60";
+                
+                game_active = false;
+
+                clearInterval(timer);
+                seconds = 60;
+                points = 0;
+            }
+
+        }, 1000); // runs every one second
+    }
 
 
 
@@ -36,7 +62,31 @@ let list = ["the","quick","brown","fox","jumps","over","the","lazy","dog"];
  * random_word()
  * 
  */
+function random_word() {
+    //generates a random number
+    let random = Math.floor(Math.random() * list.length);
+    //splits random words into it's characters
+    let word = list[random].split("");
 
+    //clear any precious words/character
+    test.innerHTML = "";
+
+    //creates the word letter by letter
+    for (let i = 0; i < word.length; i++) {
+        //create an inline span container for the letter
+        let span = document.createElement("span");
+        //creates a class that applies to all of the span that make up the word
+        span.classList.add("span");
+
+        // changes HTML of the span to show each letter
+        span.innerHTML = word[i];
+        test.appendChild(span);
+    }
+
+    //defines a variable
+    spans = document.querySelectorAll(".span");
+
+}
 
 
 
